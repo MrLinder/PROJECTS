a2cd1b78b4aa82565c5bae3e735af0641b489cb9
LinXdeep <linder.igor@gmail.com>
2016-11-07 02:42:53 +0300

    parsig texture files

--- lindXEngine/main.cpp
+++ lindXEngine/main.cpp
@@ -1,49 +1,152 @@
 #include "windows.h"
 #include <iostream>
+#include <fstream>
 using namespace std;
+/* 		ЗадачаЖ:
+	Прочитать файлы .bmp в каталоге и записать в файл, каждый файл с новой строки.
+	Если будут в каталоге появляться новые файлы то записывать их в конец файла.
+		Алгоритм:
+	Читаем перый файл и сравниваем его .txt базой 
+		если файл пустой то записываем его первым и читаем следующий
+		если файл не пустой, и имени файла нет, то записываем его в конец.
+		если файл такой есть,  то ничего не записываем и читаем дальше.
+*/
 
-template <size_t Len>
-void print(char (&arr)[Len])
+class Base {									//класс со структурой
+	protected:
+		struct Data{							//элемент списка с массивом для имен файлов
+			char* elem;
+		};
+		struct List{							//указатели
+			Data key;
+			List *next;
+			List *prev;
+			List *temp;
+			List(){next=prev=temp=NULL;}		//обнуляем указатели
+		};
+	public:
+		Base() {cout<<"Base Const"<<endl;}	
+		~Base(){cout<<"Base Distr"<<endl;}
+};
+
+class TextureParser:public Base
 {
-    cout << "len: "<<Len << endl;
-	for (size_t i = 0; i < Len; ++i)
-        cout << " "[!i] << arr[i];
-    cout << endl;
+	List* Begin;
+	List* End;
+	
+	char* mask;						//маска искомы файлов	
+	char *path;						//директория + маска для поиска файла
+	
+	WIN32_FIND_DATA FindFileData;	//Структура найденного файла
+	HANDLE h;							//дескриптор описывающий файл
+	
+	void toStack(char*);
+public:
+	TextureParser(char*);
+	~TextureParser();
+	void ScanDirectory(char* );
+	void show();
+};
+
+TextureParser::TextureParser(char* texture_file)		//инициализация парсера
+{
+	cout << texture_file<<endl;
+	
+	mask = new char[6];
+	mask = (char*)"/*.txt\0";
+	
+	Begin = End = NULL;
+	
+	fstream file((char*)texture_file, ios::in | ios::out | ios::app);
+	if (!file.is_open())
+		exit(1);
+	
+	file << 10 << " "<<123.23<<endl;
+	file << "just text ";
+	
+		
+	
+	file.close();
 }
 
-
-
-void size(char* );
-
-int main()
+TextureParser::~TextureParser()				
 {
-	WIN32_FIND_DATA FindFileData;
-	HANDLE hf;
-	hf=FindFirstFile("c:\\*", &FindFileData);
 	
-	if (hf!=INVALID_HANDLE_VALUE)
+}
+
+void TextureParser::toStack(char* name)
+{
+	int str_length = strlen(name)+1;
+	
+	//создаем элемент ни от кого не зависящий и записываем в него поступившее значение
+	int i = 0;
+	List *Temp = new List();
+		Temp->key.elem = new char[str_length];
+			for ( ; i < str_length; ++i) 
+				Temp->key.elem[i] = name[i];
+					Temp->key.elem[i+1] = '\0';
+	
+	if (Begin == NULL)		//если Указатель начала пуст значит этот элемент будет первым.
 	{
-		do
-		{
-			size(FindFileData.cFileName);
-		}
-		while (FindNextFile(hf,&FindFileData)!=0);
-		FindClose(hf);
-	}
+		Begin = Temp;
+		End = Temp;
+	} 
+	else					//если нет тогда добавляем в конец списка
+	{
+		End -> next = Temp;	
+		Temp -> prev = End;
+		End = End->next;
+	}				
 }
 
-void size(char* n)
+void TextureParser::ScanDirectory(char* dir)
 {
-	cout << n << endl;
+	path = new char[strlen(dir)+strlen(mask)+1];
+	strcpy(path, dir);
+	strcat(path, mask);
+	strcat(path, "\0");
+		
+	h = FindFirstFile(path, &FindFileData);
 	
-	char f[] ="asdasd";
-	
-	print(f);
-	
+	if (h != INVALID_HANDLE_VALUE)
+	{
+		do{
+			toStack(FindFileData.cFileName);
+		}while (FindNextFile(h,&FindFileData)!=0);
+	}
+	FindClose(h);
 }
 
 
+void TextureParser::show()
+{
+	List *p = Begin;
+	
+	do
+	{ 
+		int i = 0;
+		
+		do
+		{ 
+			  cout << p->key.elem[i];
+		} while (p->key.elem[i++] != '\0');
+		
+		 cout << endl;
+		 		 
+		 p = p->next;
+	} while (p);
+}
 
+
+int main() 
+{
+	  TextureParser *ScanTexture = new TextureParser((char*)"config_textures.txt");
+		ScanTexture -> ScanDirectory((char*)"textures");
+
+		ScanTexture -> show();
+		
+	  return 0;
+}
 
 
 
