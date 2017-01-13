8d7f80e471fa048a7b309433bd42e93791f7a61a
LindXdeep <linder.igor@gmail.com>
2017-01-13 08:53:15 +0300

    1

--- lindXEngine/main.cpp
+++ lindXEngine/main.cpp
@@ -1,39 +1,7 @@
-#include "headers/LoadParameters.h"
+#include <iostream>
+using namespace std;
 
-#ifndef MAIN_CPP    
-#define MAIN_CPP
-
-int main() 
-{
-    
-    TextureParser *TextureFolder = new TextureParser();
-                     TextureFolder -> ScanDirectory((char*)"textures");
-                     TextureFolder -> CreateTextureConfig((char*)"config_textures.txt");
-        
-    Load *Config = new Load((char*)"config_world.txt");         //Подключаем конфиг и окрываем поток
-            Config->Include(TextureFolder->ConfigFile);
-            Config->Read((char*)"Textures Path", 1);                //Прочитать и загрузить текстуры
-            Config->Read((char*)"Textures Numpollies", 2);      //Проситать и загрузить параметры полей
-            Config->Read((char*)"Textures Coordinates", 5);     //Прочитать и загрузить координаты текст
-    
-    delete TextureFolder;
-    
-    
-
-    
-    
-    
-        
-    return 0;
+int main(){
+	TextureParser *TextureFolder = new TextureParser;
+	return EXIT_SUCCESS;
 }
-
-#endif
-
-
-
-
-
-
-
-
-
