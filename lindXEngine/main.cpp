3510e3daf14b1accfc58c98d95d5cc57fdb162a8
LindXdeep <linder.igor@gmail.com>
2017-01-20 09:20:44 +0300

    files

--- lindXEngine/main.cpp
+++ lindXEngine/main.cpp
@@ -1,7 +1,14 @@
-#include <iostream>
-using namespace std;
+#include <cstring> 
+#include "headers\LoadParameters.h"
+#include "headers\LoadParameters.h"
 
 int main(){
 	TextureParser *TextureFolder = new TextureParser;
+						TextureFolder -> ScanDirectory("textures");
+												
+					delete TextureFolder;
+				
+						
+						
 	return EXIT_SUCCESS;
 }
