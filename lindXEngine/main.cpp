53bc9c61f15284908de24acfa0d7efb2d5d3653b
LindXdeep <linder.igor@gmail.com>
2017-02-10 21:07:53 +0300

    Scan and load

--- lindXEngine/main.cpp
+++ lindXEngine/main.cpp
@@ -2,18 +2,21 @@
 #include "headers\LoadParameters.h"
 #include "headers\LoadParameters.h"
 
+const char path_texture[]="textures";				//Директория в корневой папке игры в которой искать текстуры
+const char texture_file[]="config_textures.txt";	//Имя файла для базы текстур (если его нет, то будет создан)
+const char config_world[]="config_world.txt";		//Имя файла с конфигурацией мира
+
 int main(){
-	TextureParser *TextureFolder = new TextureParser;
-						TextureFolder -> ScanDirectory("textures");
-						TextureFolder -> CreateTextureConfig("config_textures.txt");
-												
-						
-						
-						
-					delete TextureFolder;
-				
-						
-						
+	TextureParser *TextureFolder = new TextureParser();
+						TextureFolder -> ScanDirectory(path_texture);
+						TextureFolder -> CreateTextureConfig(texture_file);
+		
+	Load *Config = new Load(config_world, texture_file, TextureFolder -> NumLines);					
+			Config -> ScanConfigFiles();
+			Config -> Show();
+			
+	delete TextureFolder;	
+	delete Config;					
 	return EXIT_SUCCESS;
 }
 
